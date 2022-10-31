a6f83dcdea8d768331d4e6c13bffd0232f91517b
MyUsername <alberto.baglivi@gmail.com>
2022-10-31 16:06:20 +0100

    Add the Milestone 3

--- js/script.js
+++ js/script.js
@@ -7,17 +7,28 @@ data(){
 
     return{
 
+        
         constArrayBrani : [
 
-            {nome: 'MiHerba', cantante: 'Babaman', genere: 'Dancehall', durata: 4},
-            {nome: 'No Woman No Cry' , cantante: 'Bob Marley', genere: 'Raggae', durata: 2.30},
-            {nome: 'Ganja Bus', cantante: 'Damian Marley', genere: 'Raggae', durata: 3},
-            {nome: 'Indaco dagli occhi del cielo', cantante: 'Zucchero', genere: 'Pop', durata: 2.50},
-            {nome: 'Ovunque Sarai', cantante: 'Irama', genere: 'Pop', durata: 3},
+            {nome: 'MiHerba', cantante: 'Babaman', genere: 'Dancehall', durata: 4, active: false},
+            {nome: 'No Woman No Cry' , cantante: 'Bob Marley', genere: 'Raggae', durata: 2.30,active: false},
+            {nome: 'Ganja Bus', cantante: 'Damian Marley', genere: 'Raggae', durata: 3,active: false},
+            {nome: 'Indaco dagli occhi del cielo', cantante: 'Zucchero', genere: 'Pop', durata: 2.50, active: false},
+            {nome: 'Ovunque Sarai', cantante: 'Irama', genere: 'Pop', durata: 3, active: false},
             
             ]
 
     }
+}, 
+
+methods : {
+
+    showItem(ind){
+
+     this.constArrayBrani[ind].active = !this.constArrayBrani[ind].active
+  
+    }
+
 }
 
 }).mount("#app")
