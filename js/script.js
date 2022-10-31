e6d9566350534647cc6e5f5c9c0a27d16928e558
MyUsername <alberto.baglivi@gmail.com>
2022-10-31 16:52:31 +0100

    add Milestone 4

--- js/script.js
+++ js/script.js
@@ -7,19 +7,27 @@ data(){
 
     return{
 
-        
-        constArrayBrani : [
+        song : {
+            nome : '', 
+            cantante : '', 
+            genere :'',
+            durata :'',
+            active : false
+          }, 
 
-            {nome: 'MiHerba', cantante: 'Babaman', genere: 'Dancehall', durata: 4, active: false},
-            {nome: 'No Woman No Cry' , cantante: 'Bob Marley', genere: 'Raggae', durata: 2.30,active: false},
-            {nome: 'Ganja Bus', cantante: 'Damian Marley', genere: 'Raggae', durata: 3,active: false},
-            {nome: 'Indaco dagli occhi del cielo', cantante: 'Zucchero', genere: 'Pop', durata: 2.50, active: false},
-            {nome: 'Ovunque Sarai', cantante: 'Irama', genere: 'Pop', durata: 3, active: false},
             
-            ]
+            constArrayBrani : [
 
-    }
-}, 
+                {nome: 'MiHerba', cantante: 'Babaman', genere: 'Dancehall', durata: 4, active: false},
+                {nome: 'No Woman No Cry' , cantante: 'Bob Marley', genere: 'Raggae', durata: 2.30,active: false},
+                {nome: 'Ganja Bus', cantante: 'Damian Marley', genere: 'Raggae', durata: 3,active: false},
+                {nome: 'Indaco dagli occhi del cielo', cantante: 'Zucchero', genere: 'Pop', durata: 2.50, active: false},
+                {nome: 'Ovunque Sarai', cantante: 'Irama', genere: 'Pop', durata: 3, active: false},
+                
+                ]
+
+        }
+    }, 
 
 methods : {
 
@@ -27,9 +35,20 @@ methods : {
 
      this.constArrayBrani[ind].active = !this.constArrayBrani[ind].active
   
-    }
+    }, 
 
-}
+    addSong(){
+
+        this.constArrayBrani.push({...this.song}); 
+
+        this.song.nome = '', 
+        this.song.cantante = '', 
+        this.song.genere = '',
+        this.song.durata = ''
+
+      }
+   }
+
 
 }).mount("#app")
 
